c4cc040c155ae8a546980477eee6121e5da1f22c
vinipavani <viniciusalvespavani@hotmail.com>
2020-02-06 18:45:59 -0300

    finalizações da pag de cadastro guia e turista, falta implementar o fundo e ajustar cores.

--- src/app/cadastro-guia/cadastro-guia.page.ts
+++ src/app/cadastro-guia/cadastro-guia.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-cadastro-guia',
@@ -8,10 +9,12 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 })
 export class CadastroGuiaPage implements OnInit {
 
-  registerForm = FormGroup;
+  registerForm: FormGroup;
 
-  constructor(public formbuilder: FormBuilder) {
-    
+  senhaIgual = true;
+
+  constructor(public formbuilder: FormBuilder, private storage: Storage) {
+
     this.registerForm = this.formbuilder.group({
       name: [null, [Validators.required, Validators.minLength(3)]],
       lastName: [null, [Validators.required, Validators.minLength(3)]],
@@ -19,8 +22,8 @@ export class CadastroGuiaPage implements OnInit {
       password: [null, [Validators.required, Validators.minLength(6)]],
       passwordConfirm: [null, [Validators.required, Validators.minLength(6)]],
       CPF: [null, [Validators.required, Validators.maxLength(11), Validators.minLength(11)]],
-      telefone: [null,[Validators.required, Validators.minLength(9), Validators.maxLength(9)]],
-      cadastur: [null,[Validators.required, Validators.minLength(14), Validators.maxLength(14)]]
+      telefone: [null, [Validators.required, Validators.minLength(9), Validators.maxLength(9)]],
+      cadastur: [null, [Validators.required, Validators.minLength(14), Validators.maxLength(14)]]
     });
 
   }
@@ -29,7 +32,26 @@ export class CadastroGuiaPage implements OnInit {
   }
 
   submitForm(form) {
-    console.log(form.value);
+    this.senhaIgual = form.value.passwordConfirm == form.value.password
+    let array = JSON.stringify(form.value);
+    if (this.senhaIgual == true) {
+      console.log(array);
+      console.log(form);
+      console.log(form.value);
+      this.storage.set('name', this.registerForm.value.name).then(
+        (valor) => {
+          console.log(valor);
+          this.get();
+        }
+      );
+    }
+    else {
+      return;
+    }
   }
 
+  get() {
+    this.storage.get('name');
+  }
 }
+  
