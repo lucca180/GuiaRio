39618b0832e0f00e0716bd8478742d9550479b1a
vinipavani <viniciusalvespavani@hotmail.com>
2020-02-06 01:43:18 -0200

    Conclusão do cadastro e forms do cadastro do guia ainda incompleto

--- src/app/cadastro-guia/cadastro-guia.page.ts
+++ src/app/cadastro-guia/cadastro-guia.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro-guia',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastroGuiaPage implements OnInit {
 
-  constructor() { }
+  registerForm = FormGroup;
+
+  constructor(public formbuilder: FormBuilder) {
+    
+    this.registerForm = this.formbuilder.group({
+      name: [null, [Validators.required, Validators.minLength(3)]],
+      lastName: [null, [Validators.required, Validators.minLength(3)]],
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, [Validators.required, Validators.minLength(6)]],
+      passwordConfirm: [null, [Validators.required, Validators.minLength(6)]],
+      CPF: [null, [Validators.required, Validators.maxLength(11), Validators.minLength(11)]],
+      telefone: [null,[Validators.required, Validators.minLength(9), Validators.maxLength(9)]],
+      cadastur: [null,[Validators.required, Validators.minLength(14), Validators.maxLength(14)]]
+    });
+
+  }
 
   ngOnInit() {
+  }
+
+  submitForm(form) {
+    console.log(form.value);
   }
 
 }
