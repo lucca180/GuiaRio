c29e3b4bc9f6490ed38335320c5da380b6cbd0e3
vinipavani <viniciusalvespavani@hotmail.com>
2020-02-07 22:25:37 -0300

    página de login finalizada

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 import { Storage } from '@ionic/storage';
 
@@ -10,13 +11,23 @@ import { Storage } from '@ionic/storage';
 })
 export class LoginPage implements OnInit {
 
-  constructor(private router: Router, private storage: Storage) { }
+  loginForm: FormGroup;
+
+  constructor(private router: Router, private storage: Storage, public formbuilder: FormBuilder) {
+
+      this.loginForm = this.formbuilder.group({
+          email: [null, [Validators.required, Validators.email]],
+          senha: [null, [Validators.required, Validators.minLength(6)]]
+      });
+  }
+
+  submitForm(form) {
+      console.log(form.value);
+  }
 
   ngOnInit() {
   }
 
-  submit() {
-  }
 }
   // IMPORTE DE CAMERA
 
