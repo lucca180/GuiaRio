d4b335bf55792e1c11226d23161993d262758028
vinipavani <viniciusalvespavani@hotmail.com>
2020-02-13 15:15:24 -0300

    +implementacao de toasts avisando os erros

--- Frontend/src/app/cadastro/cadastro.page.ts
+++ Frontend/src/app/cadastro/cadastro.page.ts
@@ -15,12 +15,16 @@ export class CadastroPage implements OnInit {
 
     registerForm: FormGroup;
 
+    loading: boolean = false;
+
+    errorMessage: string;
+
     senhaIgual = true;
 
     constructor( public formbuilder: FormBuilder, 
                  private storage: Storage, 
                  public router: Router, 
-                 public toastController: ToastController, 
+                 public toast: ToastController, 
                  public authService: AuthService, ) {
 
         //Código responsável pelo registro dos campos do formulário.
@@ -37,57 +41,39 @@ export class CadastroPage implements OnInit {
     ngOnInit() {
     }
 
-    //Função para o toast quando a confirmação de senha estiver incorreta.
+    //Função para o toast quando a confirmação está incorreta.
     async presentToast() {
-        const toast = await this.toastController.create({
-            message: 'Você não confirmou sua senha corretamente, tente novamente.',
-            duration: 2000
-        });
-        toast.present();
+        if (this.errorMessage){
+            const toast = await this.toast.create({
+                message: this.errorMessage,
+                duration: 2000
+            });
+            toast.present();
+        }
     }
 
     //Função repsonsável pelo envio do formulário e no comentário uma função que envia e pega o nome(nesse caso) no storage.
     //Também está responsável por levar o usuário para o login se o cadastro for válido.
 
     submitForm( form ) {
-        this.senhaIgual = form.value.passwordConfirm == form.value.password
-        let array = JSON.stringify(form.value);
-        if ( this.senhaIgual == true ) {
-            console.log( array );
-            console.log( form );
-            console.log( form.value );
-            return this.registrarUsuario( form );
-        //  this.storage.set('name', this.registerForm.value.name).then(
-        //     (valor) => {
-        //          console.log(valor);
-        //            this.get();
-        //     }
-        // );
-        }
-        //linha que chama o toast
-        else {
-            return this.presentToast();
-        }
+        return this.registrarUsuario( form );
     }
-
-    // Função que pega o nome no storage.
-
-    // get() {
-    //     this.storage.get('name');
-    // }
 
     //Função para registrar o usuário no banco de dados(integração)
 
     registrarUsuario( form ) {
         if ( form.status == "VALID" ) {
+            this.loading = true;
             this.authService.register( form.value ).subscribe(
                 ( res ) => {
-                    console.log( res );
                     this.router.navigate(['../login']);
-                }
-            );
+                },
+            e => {       
+                this.loading = false;
+                this.errorMessage = e.error.message;
+                console.error(e);
+                this.presentToast();
+            });
         }
     }
-
-
 }
--- Frontend/src/app/login/login.page.ts
+++ Frontend/src/app/login/login.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { AuthService } from './../services/auth.service';
-
+import { ToastController } from '@ionic/angular';
 
 import { Storage } from '@ionic/storage';
 
@@ -15,10 +15,15 @@ export class LoginPage implements OnInit {
 
   loginForm: FormGroup;
 
+  loading: boolean = false;
+
+  errorMessage: string; 
+
   constructor(private router: Router, 
               private storage: Storage, 
               public formbuilder: FormBuilder,
-              public authService: AuthService, ) {
+              public authService: AuthService,
+              public toast: ToastController, ) {
 
       this.loginForm = this.formbuilder.group({
           email: [null, [Validators.required, Validators.email]],
@@ -27,7 +32,6 @@ export class LoginPage implements OnInit {
   }
 
   submitForm(form) {
-      console.log(form.value);
       this.login(form);
   }
 
@@ -36,12 +40,28 @@ export class LoginPage implements OnInit {
 
   login( form ) {
       if ( form.status == "VALID" ) {
+          this.loading = true;
           this.authService.login( form.value ).subscribe(res => {
-              console.log( res.message );
               localStorage.setItem( 'userData', JSON.stringify(res.data.user) );
               localStorage.setItem( 'userToken', res.data.token );
               this.router.navigate(['../tabs/home'])
+      }, e => {       
+        this.loading = false;
+        this.errorMessage = e.error.message;
+        console.error(e);
+        this.presentToast();
       });
+    }
+  }
+
+  //Toast de erros no login.
+  async presentToast() {
+    if( this.errorMessage ) {
+      const toast = await this.toast.create({
+        message: this.errorMessage,
+        duration: 2000,
+      });
+      toast.present();
     }
   }
 
